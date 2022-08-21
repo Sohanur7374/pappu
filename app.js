11169bd2ce62dcbd5364d43d03715439cce87e20
Md. Sohanur Rahman <sohanurswe@gmail.com>
2022-08-21 14:11:23 +0600

    my first commit website

---
+++ app.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+  
+      if($(window).scrollTop() > 0){
+        $('.top').show();
+      }else{
+        $('.top').hide();
+      }
+  
+    });
+  
+    // smooth scrolling 
+  
+    $('a[href*="#"]').on('click',function(e){
+  
+      e.preventDefault();
+  
+      $('html, body').animate({
+  
+        scrollTop : $($(this).attr('href')).offset().top,
+  
+      },
+        500, 
+        'linear'
+      );
+  
+    });
+  
+  });
+  function download(fileUrl, fileName) {
+    var a = document.getElementById("filed");
+    a.href = fileUrl;
+    a.setAttribute("download", fileName);
+    a.onclick();
+  }
+  
+  download("./cv/CV_Pappu.pdf", "CV_Pappu.pdf");
